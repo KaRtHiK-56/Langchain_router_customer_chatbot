14503d9b5659460ca56b3846b655cc131d1a8bb2
Karthik <karthiksurya611@gmail.com>
2025-03-31 12:06:25 +0530

    commit

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+#import necessaary libraries
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from PyPDF2 import PdfReader
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings 
+from langchain_groq import ChatGroq 
 
+#loading the environment variables
+
+load_dotenv()
+
+HF = os.environ.get("HUGGING_FACE_API")
+GROQ = os.environ.get("GROQ_API")
+
+print("Hello")
